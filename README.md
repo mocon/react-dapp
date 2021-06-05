8b7d331bcc7d47df91938b5fd308e4580fc6e1c2
Myles O'Connor <myles.oconnor@gmail.com>
2021-06-05 12:32:41 -0700

    Read and write contract

--- README.md
+++ README.md
@@ -2,26 +2,21 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Set up Hardhat environment
+## Hardhat commands
 
-```
-npx hardhat
-```
-
-### Hardhat commands
-
-```
-npx hardhat accounts
+```shell
+npx hardhat # Set up Hardhat environment
+npx hardhat accounts # List accounts
 npx hardhat compile # Compile smart contracts
 npx hardhat test
 npx hardhat node
-npx hardhat run scripts/deploy.js --network localhost
+npx hardhat run scripts/deploy.js --network localhost # Deploy smart contracts
 npx hardhat help
 ```
 
 ## React commands
 
-```
+```shell
 yarn start # Runs the app in the development mode on http://localhost:3000
 yarn test # Launches the test runner in the interactive watch mode
 yarn build # Builds the app for production to the `build` folder
--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import './App.css'
 
-function App() {
+// Import Greeter contract ABI (see https://docs.soliditylang.org/en/v0.8.3/abi-spec.html) and address
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+const greeterAddress = '0x5fbdb2315678afecb367f032d93f642f64180aa3'
+
+export default function App() {
+  const [userGreeting, setUserGreeting] = useState('')
+
+  // Connect to user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' })
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+
+      try {
+        const data = await contract.greet()
+        console.log('🏀 data =>', data)
+      } catch (err) {
+        console.log('🏀 error =>', err)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!userGreeting) return // Do not overwrite unless there is a new greeting
+
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(userGreeting)
+      setUserGreeting('')
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className='App'>
+      <section>
+        <div>
+          <button onClick={fetchGreeting}>Fetch Greeting</button>
+        </div>
 
-export default App;
+        <div>
+          <input
+            type='text'
+            onChange={(e) => setUserGreeting(e.target.value)}
+            value={userGreeting}
+            placeholder='Greeting'
+          />
+          <button onClick={setGreeting} disabled={!userGreeting}>
+            Set Greeting
+          </button>
+        </div>
+      </section>
+    </div>
+  )
+}
