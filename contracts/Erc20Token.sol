6c5c5815ecb60c6d8efd91732957b3dde17ce29b
Myles O'Connor <myles.oconnor@gmail.com>
2021-06-06 13:04:34 -0700

    Deploy ERC20 token

--- contracts/Erc20Token.sol
+++ contracts/Erc20Token.sol
@@ -1,10 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+import "hardhat/console.sol";
+
 // See https://docs.openzeppelin.com/contracts/4.x
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MylesToken is ERC20 {
+contract Erc20Token is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 42000000 * (10**18));
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,15 +2,23 @@
 const hre = require('hardhat')
 
 async function main() {
+  // Greeter smart contract
   const Greeter = await hre.ethers.getContractFactory('Greeter')
   const greeter = await Greeter.deploy('Hello, Hardhat!')
   await greeter.deployed()
-  console.log('Greeter deployed to:', greeter.address)
+  console.log(`🏀 Greeter deployed to ${greeter.address}`)
 
+  // Token contract
   const Token = await hre.ethers.getContractFactory('Token')
   const token = await Token.deploy()
   await token.deployed()
-  console.log('Token deployed to:', token.address)
+  console.log(`🏀 Token deployed to ${token.address}`)
+
+  // ERC20 token contract
+  const Erc20Token = await hre.ethers.getContractFactory('Erc20Token')
+  const erc20Token = await Erc20Token.deploy('MylesCoin', 'MYLES')
+  await erc20Token.deployed()
+  console.log(`🏀 ERC20 token deplayed to ${erc20Token.address}`)
 }
 
 // Use async/await and handle errors
